67e6df694abd81eece00af5e26b7978a64e28704
Simon LAURENT <simon.laurent@gmx.com>
2016-04-29 08:10:18 +0200

    version finie

---
+++ LO21_CX.cpp
@@ -0,0 +1,18 @@
+//Fonction patron
+//Qt
+//Patrons de classe
+//Classe patron
+
+
+/*
+on peut lever les ambiguités an C++ avec : fonction<int||char>(a,b);
+
+Héritage entre classes patrons:
+On doit définir les types de paramètres pour l'héritage de classe patron 
+EX :*/ template<class T> class B : public A<T>;
+
+
+//typename ?
+//paramètre d'expression
+//Spécialisation
+//Librairie stantard
